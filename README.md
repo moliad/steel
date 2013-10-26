83f24ba06c19e1215b2d938175679c7c749d3342
Max <moliad@gmail.com>
2013-10-25 20:31:14 -0400

    added script to download all libs from GitHub

--- README.md
+++ README.md
@@ -14,3 +14,26 @@ if it proves easier to manage and the user base asks for it.
 
 Eventually I'll integrate some of the tools together within a unified application framework I am designing.
 
+
+slim libraries
+====
+
+In order to run the stuff in this repository, you need to get the slim library manager and the module packages
+from Github.
+
+The apps and scripts here are setup to use the libraries within a subfolder or from an external source.  
+
+There is a very simple way to get and update all the libs.  You just need to run the get-github-slim-libs.r
+Rebol script.  It is setup for SSH access by default (using Git protocol), but you can change it so that it
+uses an https connection.
+
+
+
+.gitignore file
+====
+
+The .gitignore file includes a few entries for temporary files, but it also includes one for the slim-library
+sub-folder if it's created by the get-github-slim-libs.r script.  This way the files within libs will never be
+part of the parent Git repo and won't cause any side-effects.
+
+
---
+++ get-github-slim-libs.r
@@ -0,0 +1,64 @@
+rebol [
+
+]
+
+;-------------------------
+; setup globals
+;-------------------------
+root-dir: join what-dir %slim-libs/
+
+git-connection-mode: 'ssh
+
+git-url-base: switch git-connection-mode [
+	ssh [
+		"git@github.com:moliad/" 
+	]
+	
+	https [
+		"https://github.com/moliad/" 
+	]
+]
+
+
+
+;--------------------------
+;- get-slim-package()
+;--------------------------
+get-slim-package: func [
+	package
+][
+	print "^/^/---"
+	print "pull-project()"
+	
+	path: rejoin [ root-dir package ]
+	?? path
+	
+	either exists? path [
+		print ["UPDATING " package ]
+		cmd: " git pull origin "
+		change-dir path
+		call/shell/console/wait cmd
+	][
+		print ["CLONING " package "" ]
+		cmd: rejoin [ "git clone " package ".git" ]
+		;make-dir path
+		change-dir root-dir
+		call/shell/console/wait cmd
+	]
+]
+
+
+
+
+;-------------------------
+; grab all the stuff from Github
+;-------------------------
+make-dir root-dir
+
+foreach package [ glass-libs  liquid-libs  misc-libs  utils-libs  slim  win32-libs ] [
+	get-slim-package package
+]
+
+ask "all done"
+
+
