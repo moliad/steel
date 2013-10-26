1b026951b3f9017642f693a963f9c67feb0dc190
Max <moliad@gmail.com>
2013-10-25 23:33:24 -0400

    removed debugging probes
    improved slim loading paths
    filled a header

--- header-box/header-box.r
+++ header-box/header-box.r
@@ -1,10 +1,63 @@
-rebol [
-	title: "header-box"
+rebol  [
+	; -- Core Header attributes --
+	title: "Header box - Rebol header and script management."
+	file: %header-box.r
 	version: 1.0.0
+	date: 2013-10-25
+	author: "Maxim Olivier-Adlhoch"
+	purpose: "Rebol script and header compliance tool."
+	web: http://www.revault.org
+	source-encoding: "Windows-1252"
+
+	; -- Licensing details  --
+	copyright: "Copyright © 2013 Maxim Olivier-Adlhoch"
+	license-type: "Apache License v2.0"
+	license: {Copyright © 2013 Maxim Olivier-Adlhoch
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+		http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.}
+
+	;-  / history
+	history: {
+		v1.0.0 - 2013-10-25
+			-first release.
+}
+	;-  \ history
+
+	;-  / documentation
+	documentation: {
+		Choose a spec file and then load a script.  The tool will make a GUI which replicates the spec
+		so that you can easily setup the script's header.
+		
+		It can automatically create the header so that is forces some values like license and authors.
+		
+		A few spec files are given as examples.
+		
+		Spec files can also execute some code to build values based on other header values.
+	}
+	;-  \ documentation
 ]
 
-
-do %../slim-libs/slim/slim.r
+;----
+; starts the slim library manager, if it's not setup.. the following loading setup allows slim 
+; to be installed either within the steel project or just outside of it, to be shared in multiple
+; tools.
+; 
+unless value? 'slim [
+	do any [
+		all [ exists? %../../slim-libs/slim/slim.r  %../../slim-libs/slim/slim.r ] 
+		all [ exists? %../slim-libs/slim/slim.r     %../slim-libs/slim/slim.r    ] 
+	]
+]
 
 slim/vexpose
 
@@ -491,11 +544,15 @@ add-slut-test-wrapper: funcl [
 	
 	if all [
 		string? text 
+		;probe "1"
 		ptr: find/tail text "slim-name: "
+		;probe "2"
 		slim-name: first load/next ptr
+		;probe "3"
 		new-text: copy text
+		;probe "4"
 		hdr: find new-text "slim/register"
-		
+		;probe "5"
 	][
 		;?? slim-name
 		
