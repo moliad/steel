5dba7aa832444bf041a137aac70970e809e72ab9
Max <moliad@gmail.com>
2013-11-07 22:20:17 -0500

    updated header

--- slim-libs-testing/vdump-example.r
+++ slim-libs-testing/vdump-example.r
@@ -1,6 +1,26 @@
 rebol [
-	title: "simply illustrates how vdump can map out object and block references."
+	; -- Core Header attributes --
+	title: "script to show slim's vdump capabilities"
+	file: %vdump-example.r
+	author: "Maxim Olivier-Adlhoch"
+	version: 1.0.0
+	date: 2013-11-7
+	copyright: "Copyright © 2013 Maxim Olivier-Adlhoch"
+	license: {Copyright © 2013 Maxim Olivier-Adlhoch
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+		http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.}
 ]
+
 
 
 ;----
